3239e8fe801d64917bc28f5fe41577fa61645e5d
Cameron Graybill <camerongraybill@gmail.com>
2017-02-26 02:23:20 -0500

    Please work this time!

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -4,6 +4,7 @@
 (function () {
     "use strict";
     let accessToken;
+    let initializedObjects = {};
     class User {
         constructor (userId) {
             this.id = userId;
@@ -12,7 +13,7 @@
             FB.api("/" + this.id,
                 {
                     access_token: accessToken,
-                    fields: "first_name,last_name,name,cover,picture"
+                    fields: "first_name,last_name,name,cover,picture,friends"
                 },
                 (response) => {
                     if (response && !response.error) {
@@ -22,6 +23,12 @@
                         this.cover = response.cover.source;
                         this.picture = response.picture.data.url;
                         this.feed = new Feed(this.id);
+                        this.friends = [];
+                        if (response.friends) {
+                            $.each(response.friends.data, (ignore, friend) => {
+                                this.friends.push(new User(friend.id));
+                            });
+                        }
                     } else {
                         throw new Error("Error loading User " + this.id + ": " + JSON.stringify(response));
                     }
