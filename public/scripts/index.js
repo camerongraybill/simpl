0fbf2f648bed6d0929b3a578c2c47780331e1c95
Cameron Graybill <camerongraybill@gmail.com>
2017-02-26 01:02:32 -0500

    Please work this time!

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -3,6 +3,125 @@
  */
 (function () {
     "use strict";
+    let accessToken;
+    class User {
+        constructor (userId) {
+            this.id = userId;
+        }
+        init () {
+            FB.api("/" + this.id,
+                {
+                    access_token: accessToken,
+                    fields: "first_name,last_name,name"
+                },
+                (response) => {
+                    if (response && !response.error) {
+                        this.first_name = response.first_name;
+                        this.last_name = response.last_name;
+                        this.name = response.name;
+                        this.feed = new Feed(this.id);
+                    } else {
+                        throw new Error("Error loading User " + this.id);
+                    }
+                });
+            return this;
+        }
+    }
+    class Comment {
+        constructor (commentId) {
+            this.id = commentId;
+        }
+        init () {
+            this.from = null;
+            this.message = "";
+            FB.api("/" + this.id,
+                {
+                    access_token: accessToken
+                },
+                (response) => {
+                    if (response && !response.error) {
+                        this.message = response.message;
+                        this.from = new User(response.from.id);
+                    } else {
+                        throw new Error("Error loading comment " + this.id);
+                    }
+                });
+            return this;
+        }
+    }
+    class Post {
+        constructor (postId) {
+            this.id = postId;
+        }
+
+        init () {
+            this.comments = [];
+            this.likes = [];
+            this.message = "";
+            FB.api("/" + this.id,
+                {
+                    access_token: accessToken,
+                    fields: "comments,likes,message"
+                },
+                (response) => {
+                    if (response && !response.error) {
+                        $.each(response.comments.data, (ignore, comment) => {
+                            this.comments.push(new Comment(comment.id));
+                        });
+                        $.each(response.likes.data, (ignore, like) => {
+                            this.likes.push(new User(like.id));
+                        });
+                        this.message = response.message;
+                    } else {
+                        throw new Error("Error loading post " + this.id);
+                    }
+                });
+            return this;
+        }
+    }
+    class Feed {
+        constructor (userId) {
+            this.userId = userId;
+        };
+
+        init() {
+            this.posts = [];
+            this.nextCall = "";
+            FB.api("/" + this.userId + "/feed",
+                {
+                    access_token: accessToken
+                },
+                (response) => {
+                    if (response && !response.error) {
+                        $.each(response.data, (ignore, post) => {
+                            this.posts.push(new Post(post.id));
+                        });
+                        this.nextPostsCall = response.paging.next;
+                    } else {
+                        throw new Error("Invalid User ID " + this.userId + " passed to Feed Init");
+                    }
+                });
+            return this;
+        }
+
+        morePosts() {
+            if (this.nextPostsCall) {
+                $.getJSON(this.nextPostsCall, (response) => {
+                    if (response && !response.error) {
+                        $.each(response.data, (ignore, post) => {
+                            this.posts.push(new Post(post.id));
+                        });
+                        this.nextPostsCall = response.paging.next;
+                    } else {
+                        throw new Error("Error while finding more Posts for user " + this.userId);
+                    }
+                });
+            } else {
+                throw new Error("Called for more posts when Feed has not been initialized");
+            }
+            return this;
+        }
+    }
     $(document).ready(function () {
         function initFb(callback) {
             window.fbAsyncInit = function () {
@@ -30,7 +149,9 @@
         $.getJSON("/me").done(function (userData, resObject) {
             window.userLoginData = userData;
             initFb(function () {
-                FB.api(
+                window.me = new User(userData.id);
+                console.log(window.me);
+                /*FB.api(
                     "/me/feed",
                     {
                         access_token: userLoginData.accessToken
@@ -41,11 +162,11 @@
                         }
                         $.each(response.data, (index, item) => {
                             console.log(item);
-                            FB.api("/" + item.id + "/attachments", {access_token: userLoginData.accessToken/*, fields: "id,admin_creator,application,call_to_action,caption,description,message,name,icon,from"*/}, (response) => {
+                            FB.api("/" + item.id + "/attachments", {access_token: userLoginData.accessToken/!*, fields: "id,admin_creator,application,call_to_action,caption,description,message,name,icon,from"*!/}, (response) => {
                                 console.log(response);
                             });
                         });
-                    });
+                    });*/
             });
         })
     });
