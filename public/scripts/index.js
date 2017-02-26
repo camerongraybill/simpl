9ce761321e7ff2516bf4f969bea95040d0bfec08
jenbondarchuk <14jbondarchuk@gmail.com>
2017-02-25 21:39:17 -0500

    yolo

---
+++ public/scripts/index.js
@@ -0,0 +1,43 @@
+/**
+ * Created by jenniferbondarchuk on 2/25/17.
+ */
+$(document).ready( function () {
+    function initFb(callback) {
+        window.fbAsyncInit = function () {
+            FB.init({
+                appId: '217184532019801',
+                xfbml: true,
+                version: 'v2.8'
+            });
+            FB.AppEvents.logPageView();
+            callback();
+        };
+
+        (function (d, s, id) {
+            let js, fjs = d.getElementsByTagName(s)[0];
+            if (d.getElementById(id)) {
+                return;
+            }
+            js = d.createElement(s);
+            js.id = id;
+            js.src = "//connect.facebook.net/en_US/sdk.js";
+            fjs.parentNode.insertBefore(js, fjs);
+        }(document, 'script', 'facebook-jssdk'));
+    }
+
+    $.getJSON("/me").done(function (userData, resObject) {
+        let userLoginData = userData;
+        initFb(function () {
+            FB.api(
+                "/me/feed",
+                {
+                    access_token: userLoginData.accessToken
+                },
+                function (response) {
+                    if (response && !response.error) {
+                        console.log(response)
+                    }
+                });
+        });
+    })
+});
