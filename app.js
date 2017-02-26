885803cc1613e74b765b8d2dcd2c1aababada9c4
Cameron Graybill <camerongraybill@gmail.com>
2017-02-25 20:51:59 -0500

    Please work this time!

--- app.js
+++ app.js
@@ -78,7 +78,6 @@ app.use(logger("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
 
 
 app.get("/auth/facebook", passport.authenticate("facebook"));
@@ -94,6 +93,7 @@ app.all("*", (req, res, next) => {
         next();
     }
 });
+app.use(express.static(path.join(__dirname, "public")));
 app.get("/me", (req, res) => {
     if (req.user) {
         res.json(req.user);
